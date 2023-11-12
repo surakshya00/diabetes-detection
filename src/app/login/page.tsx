3c285c91838cfdb0be4a0345eca6451dc5f58e4c
Surakshya Aryal <suhhraxya@gmail.com>
2023-11-11 19:25:15 -0500

    add basic ui component for login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,19 @@
-import { Input, Stack } from "@chakra-ui/react";
+import { Box, Button, Center, Divider, Input, Stack } from "@chakra-ui/react";
 
 export default function Login() {
   return (
     <main>
-      <h1>Authentication Page</h1>
-      <Stack spacing={3} width={300}>
-        <Input placeholder="large size" size="lg" />
-        <Input placeholder="large size" size="lg" />
-      </Stack>
+      <Box width={"100%"} height={"100vh"} display={"flex"} justifyContent={"center"} alignItems={"center"} bgColor={"black"}>
+        <Stack spacing={3} backgroundColor={"gray.700"} width={300} p={5} borderRadius={"lg"}>
+          <Input variant='outline' placeholder="Enter Email" size="lg" />
+          <Input placeholder="Enter Password" size="lg" />
+          <Button colorScheme='teal'>Log in</Button>
+          <Button colorScheme='teal' variant={"link"}>Forgot Password?</Button>
+          <Divider orientation='horizontal' />
+          <Button colorScheme='teal' variant={"link"}>New User? Create an Account</Button>
+        </Stack>
+      </Box>
+
     </main>
   );
 }
