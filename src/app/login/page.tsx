af4136042fdcd476010e7b3832d0714178398d5c
Surakshya Aryal <suhhraxya@gmail.com>
2023-11-11 19:34:57 -0500

    add hide/show functionality for password

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,29 @@
-import { Box, Button, Center, Divider, Input, Stack } from "@chakra-ui/react";
+"use client"
+import { Box, Button, Center, Divider, Input, InputGroup, InputRightElement, Stack } from "@chakra-ui/react";
+import React from "react";
+
 
 export default function Login() {
+  const [show, setShow] = React.useState(false)
+  const handleClick = () => setShow(!show)
   return (
     <main>
       <Box width={"100%"} height={"100vh"} display={"flex"} justifyContent={"center"} alignItems={"center"} bgColor={"black"}>
-        <Stack spacing={3} backgroundColor={"gray.700"} width={300} p={5} borderRadius={"lg"}>
-          <Input variant='outline' placeholder="Enter Email" size="lg" />
-          <Input placeholder="Enter Password" size="lg" />
+        <Stack spacing={3} backgroundColor={"gray.700"} width={"90%"} maxW={"400px"} p={5} borderRadius={"lg"}>
+          <Input variant='outline' placeholder="Enter Email" size="lg" color={"white"}/>
+          <InputGroup size='md'>
+            <Input
+              pr='4.5rem'
+              type={show ? 'text' : 'password'}
+              placeholder='Enter password'
+              color={"white"}
+            />
+            <InputRightElement width='4.5rem'>
+              <Button h='1.75rem' size='sm' onClick={handleClick}>
+                {show ? 'Hide' : 'Show'}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
           <Button colorScheme='teal'>Log in</Button>
           <Button colorScheme='teal' variant={"link"}>Forgot Password?</Button>
           <Divider orientation='horizontal' />
