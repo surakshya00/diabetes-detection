27c48f3c30c22b6d8f0decbc3e12a8c62efe5428
Surakshya Aryal <suhhraxya@gmail.com>
2023-11-24 02:27:44 -0500

    add signout button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,28 +2,35 @@
 import {
   Box,
   Button,
-  Center,
   Divider,
   Grid,
   GridItem,
   Heading,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Stack,
-  Text,
 } from "@chakra-ui/react";
 import React from "react";
 import Introduction from "../components/diabetes-intro";
 import ResultSummary from "../components/result-summary";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 import DiabetesFeed from "../components/diabetes-feed";
+import { signOut } from "next-auth/react";
 
 export default function Home() {
   return (
     <Box as="main" bgColor="#121212" color="white" p="5" minH="100vh">
-      <Box mt="5">
+      <Box
+        mt="5"
+        display="flex"
+        flexDir={{ base: "column", md: "row" }}
+        justifyContent="space-between"
+      >
         <Heading>Diabetes Detector</Heading>
+        <Button
+          size="sm"
+          colorScheme="teal"
+          onClick={() => signOut({ callbackUrl: "/" })}
+        >
+          Sign Out
+        </Button>
       </Box>
 
       <Divider borderColor="gray.400" w="100%" maxW="400px" my="5" />
