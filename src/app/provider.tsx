38742984b066c8cb3510a3f850fb6a6ceead7fc4
Surakshya Aryal <suhhraxya@gmail.com>
2023-11-25 01:02:09 -0500

    add unique fonts for text and heading

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -4,13 +4,14 @@
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { theme } from "./theme";
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <CacheProvider>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <QueryClientProvider client={queryClient}>
           {children}
         </QueryClientProvider>
