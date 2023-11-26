4603ba9f6d61dcdae8de1ac4044d15779a34b153
Surakshya Aryal <suhhraxya@gmail.com>
2023-11-26 16:25:48 -0500

    refactor configuration for lambda function

---
+++ lambda/config.py
@@ -0,0 +1,28 @@
+"""
+order of input features for ML model
+"""
+FEATURE_COLUMNS = [
+    "highBP",
+    "highChol",
+    "cholCheck",
+    "bmi",
+    "smoker",
+    "stroke",
+    "heartDiseaseOrAttack",
+    "physActivity",
+    "fruits",
+    "veggies",
+    "hvyAlcoholConsump",
+    "anyHealthcare",
+    "boDocbcCost",
+    "genHlth",
+    "mentHlth",
+    "physHlth",
+    "diffWalk",
+    "sex",
+    "age",
+    "education",
+    "income",
+]
+
+PATH_TO_MODEL = ""  # TODO: Pass where the model is located
--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,15 +1,12 @@
 from predictor import Predictor
 from utils import InvalidValueError, make_response, parse_event_for_features
-
-
-_FEATURE_COLUMNS = ["highBP", "highChol"]
-_PATH_TO_MODEL = ""  # TODO: Pass where the model is located
+from config import FEATURE_COLUMNS, PATH_TO_MODEL
 
 
 def lambda_handler(event, context):
     features = []
     try:
-        features = parse_event_for_features(event, _FEATURE_COLUMNS)
+        features = parse_event_for_features(event, FEATURE_COLUMNS)
     except InvalidValueError as ex:
         print(ex)
         return make_response(400, {"message": str(ex)})
@@ -18,7 +15,7 @@ def lambda_handler(event, context):
         payload = {"message": "failed to read input features"}
         return make_response(500, payload)
 
-    predictor = Predictor(_PATH_TO_MODEL)
+    predictor = Predictor(PATH_TO_MODEL)
     has_diabetes = predictor.predict(features)
 
     response = {"hasDiabetes": has_diabetes}
