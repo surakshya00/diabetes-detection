58eb0d3680184a8f34fd17a0ee350978f345970a
Surakshya Aryal <suhhraxya@gmail.com>
2023-11-26 16:26:06 -0500

    use pickle to load ml model

--- lambda/predictor.py
+++ lambda/predictor.py
@@ -1,4 +1,6 @@
-import random
+import pickle
+import numpy as np
+from sklearn.linear_model._base import LinearModel
 
 
 class Predictor:
@@ -6,13 +8,22 @@ class Predictor:
         self.model_path = model_path
         self.model = self._get_latest_model()
 
-    def _get_latest_model(self):
-        # TODO: Read model from S3/Layer
-        return {}
+    def _get_latest_model(self) -> LinearModel:
+        with open(self.model_path, "rb") as fp:
+            return pickle.load(fp)
 
     def refresh_model(self):
         self.model = self._get_latest_model()
 
     def predict(self, features) -> bool:
-        # TODO: Infer model using features
-        return random.random() < 0.5
+        result = self.model.predict(np.array([features]))
+        return result[0] == 1
+
+
+if __name__ == "__main__":
+    import random
+
+    predictor = Predictor("diabetes-detector.pkl")
+    sample_features = [random.choice([0, 1]) for _ in range(21)]
+    has_diabetes = predictor.predict(sample_features)
+    print(has_diabetes)
